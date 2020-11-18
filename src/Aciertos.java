21f4d8a02b4563af7eeae7f5fcfa582aeee3c3dc
Ariadna <ariadnasn1@hotmail.com>
2020-11-18 18:23:58 +0100

    Add comment

--- src/Aciertos.java
+++ src/Aciertos.java
@@ -4,8 +4,10 @@ public class Aciertos {
 
     public static void main(String[] args) {
         int n = 0,cont = 0;
+        // Aqui restamos el minimo al maximo +1 para que el random siempre sea desde el 1 hasta el 40 y luego
+        // le sumamos 10 para que cumpla la condicion de ser siempre de 10 a 50
         int x = (int) ((50 -10 +1) *Math.random()+10);
-        //System.out.print(x);
+
         Scanner sc = new Scanner(System.in);
         do {
             System.out.print("\nIntroduce el valor de X: ");
